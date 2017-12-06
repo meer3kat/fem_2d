1eb7d435b0c7a99a5b6bd846a528b917b7baebd1
meer3kat <peiliguo@gmail.com>
2017-12-06 04:07:37 +0100

    cleaned up for solving B1 and plot error analysis of energy norm vs. meshsize and plot of coarest and finest mesh.

--- my2dsolver.m
+++ my2dsolver.m
@@ -1,32 +1,35 @@
 clear all;
+% to call include
+%assemble.m
+%f1.m 
+%g1.m
+%in this script, we will solve
+% -u'' = f in 2D.
+%first initialize our grid
+
 geometry = @circleg;
-%{
-hmax = 0.1;
-[p, e, t] = initmesh(geometry, 'hmax', hmax);
-
-
-
-[A, R, b, r] = assemble(p, e, t, @f1, @g1, 1);
-%b = load_vector_2d(p, t, @f1);
-
-phi = (A+R)\(b+r);
-
-for i = 1:length(phi)
-    solution = g1(p(1,i),p(2,i));
-    %sin(2*pi*p(1,i))*sin(2*pi*p(2,i));
-    err(i) = solution - phi(i);
-end
-
-ene = sqrt(err*A*err');
-
-pdecont(p, t, phi);
-%}
+% a unit circle. 
+% we will test it with different size of mesh, 
 
 for j = 1:5
     h(j) = 1/(2^j);
     [p,e,t] = initmesh(geometry,'hmax', h(j));
-    [A, R, b, r] = assemble(p, e, t, @f1, @g1, 1);
-    phi = (A+R)\(b+r);
+    [A, M, R, b, r] = assemble(p, e, t, @f1, @g1, 1);
+    phi = (A+R)\(b+r); %this is the FEM results.
+    
+    
+    if h(j) == 0.5
+        pp1 = p;
+        tt1 = t;
+        xx1 = phi;
+    end
+    
+    if h(j) == 1/32
+        pp2 = p;
+        tt2 = t;
+        xx2 = phi;
+    end
+        
     
     for i = 1:length(phi)
         
@@ -35,10 +38,39 @@ for j = 1:5
     
     end
     
-    enex = sqrt(err*A*err');
+    enex = sqrt(err*A*err'); %energy norm
     totalerr(j) = enex;
-    %pdesurf(p,t,phi);
-    %waitforbuttonpress
- 
-    
+        
 end
+
+
+%plot of energy norm 
+linearfits = polyfit(log(h), log(totalerr), 1);
+x = [0 -3.5];
+fit = linearfits(1)*x + linearfits(2);
+scatter(log(h), log(totalerr));
+hold on;
+plot(x, fit);
+text(-1.5,0,['slope = ',num2str(linearfits(1))]);
+xlabel('log(h)');
+
+ylabel('log(error)');
+title('error analysis and convergence rate for energy norm');
+legend('error in the form of energy norm');
+grid on;
+saveas(gcf,'energynorm.png');
+hold off;
+
+%plot of the solution 
+fig = figure('position', [0,0,1000,450]);
+subplot(1,2,1);
+pdesurf(pp1,tt1,xx1);
+title('solution with mesh size 1/2');
+
+subplot(1,2,2);
+pdesurf(pp2,tt2,xx2);
+title('solution with mesh size 1/32');
+
+saveas(fig, '2dsolution.png');
+ 
+
